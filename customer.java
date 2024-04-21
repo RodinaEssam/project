65eb309b645e966162dcf7ac612e83283eeaf09e
RodinaEssam <167621007+RodinaEssam@users.noreply.github.com>
2024-04-21 22:51:27 +0200

    Update customer.java

--- customer.java
+++ customer.java
@@ -5,7 +5,6 @@ private int customerId;
 private String name;
 private String address;
 public customer(int customerId, String name, String address) {
-	super();
 	this.customerId = customerId;
 	this.name = name;
 	this.address = address;
